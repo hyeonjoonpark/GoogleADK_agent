58976a05464ec8cb292ffa739961b0422a55635b
hyeonjoonpark <pjjoon1379@gmail.com>
2025-08-09 14:29:22 +0900

    FEAT: capital_agent/agent.py

--- agents/capital_agent/agent.py
+++ agents/capital_agent/agent.py
@@ -1,8 +1,16 @@
 from google.adk.agents.llm_agent import Agent
 
+def get_capital_city(country: str) -> str:
+    """Retrieve the capital city of a given country."""
+    capitals = {"korea": "Seoul", "canada": "Ottawa"}
+    return capitals.get(country.lower(), f"Sorry, I don't know the capital of {country}.")
+
+# Create the root agent
+# 무조건 변수가 root_agent 이어야 한다
 root_agent = Agent(
     model='gemini-2.5-flash',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    name='capital_agent',
+    description='Answer questions about capital cities.',
+    instruction='You are a helpful assistant that provides information about capital cities.',
+    tools=[get_capital_city] # agent tool
 )
